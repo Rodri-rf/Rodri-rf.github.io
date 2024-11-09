3a976b9d71f7d4f996f0dc2334aba4cf7fa70e9a
Rodri-rf <106401275+Rodri-rf@users.noreply.github.com>
2024-11-09 14:46:20 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,3 +24,20 @@ const observer = new IntersectionObserver((entries) => {
 sections.forEach(section => {
     observer.observe(section);
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('projects.json')
+        .then(response => response.json())
+        .then(data => {
+            const projectContainer = document.querySelector('#projects');
+            data.forEach(project => {
+                const projectElement = document.createElement('div');
+                projectElement.classList.add('project');
+                projectElement.innerHTML = `
+                    <h3><a href="${project.link}" target="_blank">${project.title}</a></h3>
+                    <p>${project.description}</p>
+                `;
+                projectContainer.appendChild(projectElement);
+            });
+        });
+});
